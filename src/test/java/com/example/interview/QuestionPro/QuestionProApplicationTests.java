91005bdb0771775dbd38ac43a0c7f92fef46bbb3
Akshay Aher <akshay.aher@atyeti.com>
2023-04-28 14:05:19 +0530

    Initial commit

---
+++ src/test/java/com/example/interview/QuestionPro/QuestionProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.interview.QuestionPro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
